490a6d57ae10d017236ae781e0b0194ae625d094
GingerWookiee <97709330+GingerWookiee@users.noreply.github.com>
2022-02-12 15:48:13 -0500

    changes

--- itis3135/scripts/firstjs.js
+++ itis3135/scripts/firstjs.js
@@ -3,6 +3,11 @@ function scriptTest()
     alert("Hey my script is running");
 }
 
+/*
+ * from here to the next big comment is about the welcome message 
+*/
+
+//global variables for name and feeling
 var name;
 var feeling;
 
@@ -15,6 +20,7 @@ function welcomeWords()
     + getName() + "!  We're glad to hear you are feeling " + getFeeling() + "!";
 }
 
+//setter and getter for name
 function setName(name){ 
     this.name = name;
 }
@@ -22,6 +28,7 @@ function getName() {
     return name;
 }
 
+//setter and getter for feeling
 function setFeeling(feeling) {
     this.feeling = feeling;
 }
@@ -29,19 +36,27 @@ function getFeeling() {
     return feeling;
 }
 
-document.getElementById('date').innerHTML = "It is " + getTime() + " On " + getDate();
+/*
+ * from here to the next big comment is about the date 
+*/
 
-function getTime() {
-    let date = new Date();
-    let ampm = date.getHours() >= 12 ? 'PM' : 'AM';
-    let mins = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
-    return date.getHours() % 12 + ':' + mins + ' ' + ampm;
-}
-function getDate() {
+document.getElementById('date').innerHTML = "It is " + getCurrentTime() + " On " + getCurrentDate();
+
+function getCurrentDate() {
     let date = new Date();
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     return days[date.getDay()] + ', ' + months[date.getMonth()] + ' ' + date.getDate();
 }
 
+function getCurrentTime() {
+    let date = new Date();
+    let ampm = date.getHours() >= 12 ? 'PM' : 'AM';
+    let mins = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+    return date.getHours() % 12 + ':' + mins + ' ' + ampm;
+}
 
+
+/*
+ * from here to the next big comment is about 
+*/
