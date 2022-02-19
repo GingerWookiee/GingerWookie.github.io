34ca1f4785d846e0c667659b19f2fa92e0021e10
GingerWookiee <97709330+GingerWookiee@users.noreply.github.com>
2022-02-19 17:25:21 -0500

    changes

--- itis3135/scripts/activity6js.js
+++ itis3135/scripts/activity6js.js
@@ -4,7 +4,7 @@ var shapeName;
 
 function findShapeName()
 {
-    setSides(parseFloat(prompt("Enter your number between 0 and 10:")));
+    sideCount = parseFloat(prompt("Enter your number between 0 and 10:"));
 
     while(i == 0)
     {
@@ -14,72 +14,59 @@ function findShapeName()
         }
         else
         {
-            setSides(parseFloat(prompt("You did not enter a number, please enter a number between 0 and 10:")));
+            sideCount = parseFloat(prompt("You did not enter a number, please enter a number between 0 and 10:"));
         }
     }
-    setShape(getSides());
-    document.getElementById("polygons").innerHTML = "A shap with " + getSides() + " sides is called a(n) " + getShape() + "."; 
-}
-
-function setSides(newSides)
-{
-    this.sideCount = newSides;
-}
-function getSides()
-{
-    return sideCount;
+    shapeName = setShape(sideCount);
+    document.getElementById("polygons").innerHTML = "A shap with " + sideCount + " sides is called a(n) " + getShape() + "."; 
 }
 
 function setShape(sides)
 {
+    sides = Math.round(Math.abs(sides));
     switch (sides) 
     {
         case 0:
-            shapeName = "nonexistant shape";
+            return "A polygon with this many sides does not exist :(";
             break;
         case 1:
-            shapeName = "monogon";
+            return "A polygon with 1 side is a Henagon";
             break;
         case 2:
-            shapeName = "digon";
+            return "A polygon with 2 side is a Digon";
             break;
         case 3:
-            shapeName = "triangle";
+            return "A polygon with 3 side is a Trigon";
             break;
         case 4:
-            shapeName = "quadrilateral";
+            return "A polygon with 4 side is a Tetragon";
             break;
         case 5:
-            shapeName = "pentagon";
+            return "A polygon with 5 side is a Pentagon";
             break;
         case 6:
-            shapeName = "hexagon";
+            return "A polygon with 6 side is a Hexagon";
             break;
         case 7:
-            shapeName = "septagon";
+            return "A polygon with 7 side is a Heptagon";
             break;
         case 8:
-            shapeName = "octagon";
+            return "A polygon with 8 side is a Octagon";
             break;
         case 9:
-            shapeName = "nonagon";
+            return "A polygon with 9 side is a Nonagon";
             break;
         case 10:
-            shapeName = "decagon";
+            return "A polygon with 10 side is a Decagon";
             break;
     }
-}
-
-function getShape()
-{
-    return shape;
 }
 
 function validateInput(sides) 
 {
     if (typeof sides == 'number') 
     {
-        if( sides >= -10 && sides <= 10) 
+        if (sides >= -10 && sides <= 10) 
         {
             return (sides == sides);
         }
