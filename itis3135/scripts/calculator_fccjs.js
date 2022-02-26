f0998330f3164deeeeafcc0f6904923ba07e0348
GingerWookiee <97709330+GingerWookiee@users.noreply.github.com>
2022-02-26 11:35:49 -0500

    changes

--- itis3135/scripts/calculator_fccjs.js
+++ itis3135/scripts/calculator_fccjs.js
@@ -1,13 +1,21 @@
 window.onload = function () {
-    const calculate = (n1, operator, n2) => {
+    const calculate = (n1, operator, n2) => 
+    {
         let result = ''
-        if (operator === 'add') {
+        if (operator === 'add') 
+        {
           result = parseFloat(n1) + parseFloat(n2)
-        } else if (operator === 'subtract') {
+        } 
+        else if (operator === 'subtract') 
+        {
           result = parseFloat(n1) - parseFloat(n2)
-        } else if (operator === 'multiply') {
+        } 
+        else if (operator === 'multiply') 
+        {
           result = parseFloat(n1) * parseFloat(n2)
-        } else if (operator === 'divide') {
+        } 
+        else if (operator === 'divide') 
+        {
           result = parseFloat(n1) / parseFloat(n2)
         }
       
@@ -18,8 +26,10 @@ window.onload = function () {
       const display = calculator.querySelector('.calculator__display')
       const keys = calculator.querySelector('.calculator__keys')
       
-      keys.addEventListener('click', function (e) {
-        if (e.target.matches('button')) {
+      keys.addEventListener('click', function (e) 
+      {
+        if (e.target.matches('button')) 
+        {
           const key = e.target
           const action = key.dataset.action
           const keyContent = key.textContent
@@ -29,15 +39,20 @@ window.onload = function () {
           Array.from(key.parentNode.children)
             .forEach(k => k.classList.remove('is-depressed'))
       
-          if (!action) {
-            if (displayedNum === '0' || previousKeyType === 'operator') {
+          if (!action) 
+          {
+            if (displayedNum === '0' || previousKeyType === 'operator') 
+            {
               display.textContent = keyContent
-            } else {
+            } 
+            else 
+            {
               display.textContent = displayedNum + keyContent
             }
           }
       
-          if (action === 'decimal') {
+          if (action === 'decimal') 
+          {
             display.textContent = displayedNum + '.'
           }
       
@@ -46,18 +61,21 @@ window.onload = function () {
             action === 'subtract' ||
             action === 'multiply' ||
             action === 'divide'
-          ) {
+            ) 
+          {
             key.classList.add('is-depressed')
             calculator.dataset.previousKeyType = 'operator'
             calculator.dataset.firstValue = displayedNum
             calculator.dataset.operator = action
           }
       
-          if (action === 'clear') {
-            console.log('clear key!')
+          if (action === 'clear') 
+          {
+            calculator.dataset.previousKeyType = 'clear'
           }
       
-          if (action === 'calculate') {
+          if (action === 'calculate') 
+          {
             const firstValue = calculator.dataset.firstValue
             const operator = calculator.dataset.operator
             const secondValue = displayedNum
