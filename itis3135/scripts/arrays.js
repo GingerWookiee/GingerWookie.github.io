21566280cf88dc3c7ef9976616240469209fb23e
GingerWookiee <97709330+GingerWookiee@users.noreply.github.com>
2022-03-06 08:38:36 -0500

    changes

---
+++ itis3135/scripts/arrays.js
@@ -0,0 +1,18 @@
+var names = ["Daniel", "Bobby", "Isabel", "Tim", "Gracie"];
+var salary = [0, 0, 0, 0, 0];
+
+
+function addSalary()
+{
+
+}
+
+function displaySalary()
+{
+
+}
+
+function displayResults()
+{
+
+}
