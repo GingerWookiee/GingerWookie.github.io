f01d24c97ed97b852889e5518bb4f4b31d24b9a9
GingerWookiee <97709330+GingerWookiee@users.noreply.github.com>
2022-04-02 16:23:14 -0400

    changes

---
+++ itis3135/project/scripts/interior_exterior.js
@@ -0,0 +1,9 @@
+function planScroll() 
+{
+    document.getElementById("plan").scrollIntoView({behavior: 'smooth'});
+}
+
+function topScroll()
+{
+    document.getElementById("top").scrollIntoView({behavior: 'smooth'});
+}
