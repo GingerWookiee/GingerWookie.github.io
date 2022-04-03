ed52f9cd86797faa7fc3b8e9934a5b5bde7f9fea
GingerWookiee <97709330+GingerWookiee@users.noreply.github.com>
2022-04-03 13:28:43 -0400

    changes

--- itis3135/project/scripts/timeline.js
+++ itis3135/project/scripts/timeline.js
@@ -24,13 +24,13 @@ function costUp()
 //Time Functionality
 function calculate()
 {
-    var part1 = document.getElementById("part1");
-    var part2 = document.getElementById("part2");
-    var part3 = document.getElementById("part3");
-    var part4 = document.getElementById("part4");
-    var part5 = document.getElementById("part5");
-    var part6 = document.getElementById("part6");
-    var part7 = document.getElementById("part7");
+    var part1 = document.getElementById("part1").value;
+    var part2 = document.getElementById("part2").value;
+    var part3 = document.getElementById("part3").value;
+    var part4 = document.getElementById("part4").value;
+    var part5 = document.getElementById("part5").value;
+    var part6 = document.getElementById("part6").value;
+    var part7 = document.getElementById("part7").value;
 
     var sum = part1 + part2 + part3 + part4 + part5 + part6 + part7;
     var weeks = sum / 7;
