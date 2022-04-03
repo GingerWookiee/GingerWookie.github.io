b5cc46ec21fcbed4b75278b5b549b91102e05a85
GingerWookiee <97709330+GingerWookiee@users.noreply.github.com>
2022-04-03 13:40:03 -0400

    changes

--- itis3135/project/scripts/timeline.js
+++ itis3135/project/scripts/timeline.js
@@ -24,16 +24,16 @@ function costUp()
 //Time Functionality
 function calculate()
 {
-    var part1 = document.getElementById("part1").value;
-    var part2 = document.getElementById("part2").value;
-    var part3 = document.getElementById("part3").value;
-    var part4 = document.getElementById("part4").value;
-    var part5 = document.getElementById("part5").value;
-    var part6 = document.getElementById("part6").value;
-    var part7 = document.getElementById("part7").value;
+    var part1 = paresInt(document.getElementById("part1").value);
+    var part2 = parseInt(document.getElementById("part2").value);
+    var part3 = parseInt(document.getElementById("part3").value);
+    var part4 = parseInt(document.getElementById("part4").value);
+    var part5 = parseInt(document.getElementById("part5").value);
+    var part6 = parseInt(document.getElementById("part6").value);
+    var part7 = parseInt(document.getElementById("part7").value);
 
     var sum = part1 + part2 + part3 + part4 + part5 + part6 + part7;
-    var weeks = sum / 7;
+    var weeks = parseInt(sum / 7);
     var weekdays = sum % 7;
 
     document.getElementById("result").innerHTML = "<p>Your total number of days of work is " + sum.toString() + " days, <br> or " + weeks.toString() + " weeks and " + weekdays.toString() + " days.</p>";
