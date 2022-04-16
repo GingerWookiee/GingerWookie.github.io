fa53232250c624453956479de3ad507e3103da08
GingerWookiee <97709330+GingerWookiee@users.noreply.github.com>
2022-04-16 14:35:18 -0400

    changes

---
+++ itis3135/Activity13/teague_Activity13.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $.ajax({
+        type: "get",
+        url: "team.json",
+        beforeSend: function() 
+        {
+            $("#team").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) 
+        {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) 
+        {
+            $("#team").html("");
+            $.each(data, function() 
+            {
+                $.each(this, function (key, value) 
+                {
+                    $("#team").append
+                    (
+                        "<h3>Name: " + value.name + "</h3><br>" +
+                        "Title: " + value.title + "<br>" +
+                        "Bio: " + value.bio + "<br><br>"
+                    );
+                });
+            });
+        }
+    });
+});
