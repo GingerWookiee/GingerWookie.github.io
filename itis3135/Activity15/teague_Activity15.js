e3794b0944de1956124caacd380339e08f4465ef
GingerWookiee <97709330+GingerWookiee@users.noreply.github.com>
2022-04-23 16:53:18 -0400

    changes

--- itis3135/Activity15/teague_Activity15.js
+++ itis3135/Activity15/teague_Activity15.js
@@ -8,7 +8,7 @@ $(document).ready(function()
             {
                 $("#faculty").append
                 (
-                    "<img src=" + " " + value.image + "><br>" +
+                    "<img src=" + " " + value.image + ">" +
                     "<h2>" + value.full_name + "</h2>" +
                     "<h3>" + value.department + "</h3>" +
                     "<p>" + value.bio + "</p>"
