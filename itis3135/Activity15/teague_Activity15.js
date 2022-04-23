eef64a4f9ee83f1107c7ee1a45bcfad380d7dcf4
GingerWookiee <97709330+GingerWookiee@users.noreply.github.com>
2022-04-23 16:39:04 -0400

    changes

--- itis3135/Activity15/teague_Activity15.js
+++ itis3135/Activity15/teague_Activity15.js
@@ -1,33 +1,19 @@
-$(document).ready(function(){
-        $.ajax({
-            type: "get",
-            url: "json_files/facultyList.json",
-            beforeSend: function() 
+$(document).ready(function()
+{
+    $.getJSON("facultyList.json", function (data)
+    {
+        $.each(data, function ()
+        {
+            $.each(this, function (key, value)
             {
-                $("#faculty").html("Loading...");
-            },
-            timeout: 10000,
-            error: function(xhr, status, error) 
-            {
-                alert("Error: " + xhr.status + " - " + error);
-            },
-            dataType: "json",
-            success: function(data) 
-            {
-                $("#faculty").html("");
-                $.each(data, function() 
-                {
-                    $.each(this, function (key, value) 
-                    {
-                        $("#faculty").append
-                            (
-                            "<img src= \'" + value.image + "\'><br>" +
-                            "<h2>" + value.full_name + "</h2>" +
-                            "<h3>" + value.department + "</h3>" +
-                            "<p>" + value.bio + "</p>"
-                            );
-                    });
-                });
-            }
+                $("#faculty").append
+                (
+                    "<img src=" + " " + value.image + "><br>" +
+                    "<h2>" + value.full_name + "</h2>" +
+                    "<h3>" + value.department + "</h3>" +
+                    "<p>" + value.bio + "</p>"
+                );
+            });
         });
+    });
 });
