1cdef8f8044f5e85b406b7a249979c977de63d05
GingerWookiee <97709330+GingerWookiee@users.noreply.github.com>
2022-04-24 08:38:15 -0400

    changes

--- itis3135/project/scripts/links.js
+++ itis3135/project/scripts/links.js
@@ -1,3 +1,12 @@
 $(document).ready(function() { 
-    $(".livepreview").livePreview();
+    $(".livepreview").livePreview({
+        trigger: 'hover',
+        viewWidth: 300,  
+        viewHeight: 200,  
+        targetWidth: 1000,  
+        targetHeight: 800,  
+        scale: '0.5', 
+        offset: 50,
+        position: 'left'
+    });
   });
